96f70a0f9775a7b801eb7e0ffa18dbcbe81ddb5d
daniel-alencar-br <danielalencar.br@gmail.com>
2023-10-30 20:26:13 -0300

    Adicionar arquivos de projeto.

---
+++ TesteAPISwagger/Controllers/Funcoes.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteAPISwagger.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Funcoes : ControllerBase
+    {
+        [HttpGet("{Valor}")]
+        public int Get(int Valor)
+        {
+            return (2 * Valor);
+        }
+    }
+}
